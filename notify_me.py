a53084e5e41aa40bc9d929581b534a8df502afc3
limner <manan.mehta2@gmail.com>
2020-06-01 20:00:28 +0530

    first commit

---
+++ notify_me.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from IPython.core.magic import register_cell_magic
+from IPython.display import Audio, display
+import time
+
+
+@register_cell_magic
+def notify_me(line,cell):
+    start = time.time()
+    try:
+       exec(cell)
+       end = time.time()
+       print(end - start)
+       display(Audio(url='https://sound.peal.io/ps/audios/000/000/537/original/woo_vu_luvub_dub_dub.wav', autoplay=True))
+    except Exception as e:
+       print(type(e).__name__, " : ",e)
+       end = time.time()
+       print(end - start)
+       display(Audio(url='https://sound.peal.io/ps/audios/000/000/537/original/woo_vu_luvub_dub_dub.wav', autoplay=True))
+       
